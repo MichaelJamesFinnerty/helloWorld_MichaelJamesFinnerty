02ff1b6fc6eb3b9172319d3b8b5644c3118b934e
michaelfinnerty <michaelfinnerty@localhost>
2022-05-16 20:42:47 -0600

    artistic expression
    none of your fucking business

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,3 +1,3 @@
 import com.neuronrobotics.bowlerstudio.BowlerKernel
 // Have BowlerStudio speak messages
-BowlerKernel.speak("Welcome to Bowler Studio")
+BowlerKernel.speak("Welcome to Bowler Studio", 250.0, 100.0, 40.0, 1.0, 1.0)
